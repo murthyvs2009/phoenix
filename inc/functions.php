3f4048a0c5c1c0a7ada64d1dcab852e7ba719fba
Vemuri S Murthy <murthyvs2009@gmail.com>
2023-02-07 19:30:54 +0530

    slugify Function updated]

--- inc/functions.php
+++ inc/functions.php
@@ -14,10 +14,32 @@ define("CMSCONTENT", "content/");
 
 
 //Converts string to slug 
-function vsmSlugify($fName){
-    $fName=strtolower($fName);
-    $fName=str_replace(" ","_",$fName);
-return $fName;
+function vsmSlugify($text, string $divider = '_'){
+
+
+ // replace non letter or digits by divider
+  $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+  // remove unwanted characters
+  $text = preg_replace('~[^-\w]+~', '', $text);
+
+  // trim
+  $text = trim($text, $divider);
+
+  // remove duplicate divider
+  $text = preg_replace('~-+~', $divider, $text);
+
+  // lowercase
+  $text = strtolower($text);
+
+  if (empty($text)) {
+    return 'n-a';
+  }
+
+  return $text;
 }
 
 
